d74aae8643ef26db47b318ad281ad759a240e137
Vitaly Kharlakov <vkharlakov@yandex.ru>
2022-08-20 18:54:32 +0300

    Исправлены замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,28 @@
 let popUpForm = document.querySelector('.edit-form')
 let closeButton = document.querySelector('.edit-form__close-button')
 let editButton = document.querySelector('.profile__edit-button')
-
-function popUp() {
-	if (popUpForm.classList.contains('edit-form_opened') === false) {
-		inputName.value = profileName.innerHTML
-		inputBrief.value = profileBrief.innerHTML
-		popUpForm.classList.add('edit-form_opened')
-	}
-	else {
-		popUpForm.classList.remove('edit-form_opened')
-	}
-}
-
-editButton.addEventListener('click', popUp)
-closeButton.addEventListener('click', popUp)
-
 let profileName = document.querySelector('.profile__name')
 let profileBrief = document.querySelector('.profile__brief')
 let inputName = document.querySelector('.edit-form__input_type_name')
 let inputBrief = document.querySelector('.edit-form__input_type_brief')
-let submitButton = document.querySelector('.edit-form__submit-button')
 
-inputName.value = 'Жак-Ив Кусто'
-inputBrief.value = 'Исследователь океана'
-
-function submitForm(evt) {
-	profileName.innerHTML = inputName.value
-	profileBrief.innerHTML = inputBrief.value 
-	popUp()
+function showPopUp() {
+	inputName.value = profileName.textContent
+	inputBrief.value = profileBrief.textContent
+	popUpForm.classList.add('edit-form_opened')
 }
 
-let inputBox = document.querySelectorAll('.edit-form__input')
+function hidePopUp() {
+	popUpForm.classList.remove('edit-form_opened')
+}
 
-inputBox.forEach(inputBox => {
-	inputBox.addEventListener('keyup', function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        submitButton.click();
-    }
-})
-})
+function submitForm(evt) {
+	event.preventDefault();
+	profileName.textContent = inputName.value
+	profileBrief.textContent = inputBrief.value 
+	hidePopUp()
+}
 
-submitButton.addEventListener('click', submitForm)
+editButton.addEventListener('click', showPopUp)
+closeButton.addEventListener('click', hidePopUp)
+popUpForm.addEventListener('submit', submitForm)
